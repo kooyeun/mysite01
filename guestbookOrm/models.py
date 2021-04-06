3322205a26afd0c97400b309e29d900365115b4c
kooyeun <go.orRun.straight@gmail.com>
2021-04-06 19:30:22 +0900

    guestbookORM #2 models 세팅

--- guestbookOrm/models.py
+++ guestbookOrm/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Guestbook(models.Model):
+    name =models.CharField(max_length=45)
+    password =models.CharField(max_length=45)
+    message =models.TextField
+    regdate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Guestbook({self.name},{self.password},{self.message},{self.regdate})'
